1eacfca3884bdcecbc32cbeec07d5b2460e179e8
a4510sei <asei4510you@gmail.com>
2024-07-24 22:16:54 +0900

    プロフィール編集画面作成

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,7 +1,29 @@
+<!-- layouts/login.blade呼び出し  -->
 @extends('layouts.login')
-
+<!-- posts/index.blade呼び出し  -->
+<!-- contentセクション  -->
 @section('content')
 
+{!! Form::open(['url' => '/profile']) !!}
+<!-- ユーザー名-->
+{{ Form::label('username') }}
+{{ Form::text('username',null,['class' => 'input']) }}
+<br>
+<!-- アドレス-->
+{{ Form::label('mail') }}
+{{ Form::text('mail',null,['class' => 'input']) }}
+<br>
+<!-- パスワード-->
+{{ Form::label('password') }}
+{{ Form::password('password',['class' => 'input']) }}
+<br>
+<!-- パスワード確認-->
+{{ Form::label('password') }}
+{{ Form::password('password',['class' => 'input']) }}
+<br>
+<!-- 自己紹介-->
+{{ Form::label('〇〇') }}
+{{ Form::text('〇〇',null,['class' => 'input']) }}
 
 
 @endsection
